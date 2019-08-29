57189915e15685d39f2ffe8fa6aaecdfac50fe77
prince_kumar <prince_kumar@BRD024676>
2019-08-29 17:57:02 +0530

    Add project files.

---
+++ EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmpolyeeDataAccess;
+using EmployeeService.Models;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+       
+
+        public HttpResponseMessage Post ([FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri +"/"+ employee.ID.ToString());
+
+                    return message;
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+
+    }
+}
